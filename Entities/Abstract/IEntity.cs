f6148c86e9588eba00ac224fcc3e3b95fab53f80
Tutku ÖZKAL <tutku.ozkal@gmail.com>
2021-01-30 22:56:18 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity Implement eden bir class veri tabanı
+    public interface IEntity
+    {
+    }
+}
